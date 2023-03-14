221eb7ab91e3edec76ab0154c93976f62797e518
eddie ashton <eddie.ashton@madetech.com>
2023-03-14 10:54:43 +0000

    completed lasagna

--- lasagna/src/main/java/Lasagna.java
+++ lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,19 @@
 public class Lasagna {
     // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven(){
+        return 40;
+    }
 
     // TODO: define the 'remainingMinutesInOven()' method
-
+    public int remainingMinutesInOven(int minsInOven){
+        return expectedMinutesInOven() - minsInOven;
+    }
     // TODO: define the 'preparationTimeInMinutes()' method
-
+    public int preparationTimeInMinutes(int amountOfLayers){
+        return amountOfLayers * 2;
+    }
     // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes( int amountOfLayers, int minsInOven){
+        return preparationTimeInMinutes(amountOfLayers) + minsInOven;
+    }
 }
